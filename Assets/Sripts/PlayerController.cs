53971f43c672ecf814c330dad88398cf7897245d
Alex-glitch2 <ALexa187@nycstudents.net>
2024-10-16 09:22:39 -0400

    changes for the game

--- Assets/Sripts/PlayerController.cs
+++ Assets/Sripts/PlayerController.cs
@@ -5,39 +5,20 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    
+    public InputAction moveAction; 
     // Start is called before the first frame update
     void Start()
     {
-        
+    moveAction.Enable();  
     }
 
     // Update is called once per frame
     void Update()
     {
-     float horizontal = 0.0f;
-     if   (Keyboard.current.leftArrowKey.isPressed)
-      {
-      horizontal = -1.0f;
-      }
-      else if  (Keyboard.current.rightArrowKey.isPressed)
-      {
-      horizontal = 1.0f;
-      }
-      Debug.Log(horizontal);
-
-      float vertical = 0.0f;
-      if  (Keyboard.current.downArrowKey.isPressed)
-      {
-      vertical = -1.0f;
-      }
-      else if  (Keyboard.current.upArrowkey.isPressed)
-      {
-      vertical = 1.0f; 
-      }
-      Debug.Log(vertical);
-
-      Vector2 position = transform.position;
-      position.x = position.x + 0.1f * horizontal;
-      transform.position = position; 
+      Vector2 move = moveAction.ReadValue<Vector2>();
+      Debug.Log(move); 
+      Vector2 position = (Vector2)transform.position + move * 0.1f;
+      transform.position = position;  
     }
 }     
