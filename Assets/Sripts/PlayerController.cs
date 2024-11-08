1e32a92f9fca085a4d73fab39885e98a2a0b2a72
Alex-glitch2 <ALexa187@nycstudents.net>
2024-11-08 08:41:59 -0500

    changes

--- Assets/Sripts/PlayerController.cs
+++ Assets/Sripts/PlayerController.cs
@@ -6,23 +6,28 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     
+    
+    // Variables related to player character movement 
     public InputAction moveAction; 
+    Rigidbody2D rigidbody2d;
+    Vector2 move; 
     // Start is called before the first frame update
     void Start()
     {
-    moveAction.Enable();  
+    moveAction.Enable(); 
+    rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-      Vector2 move = moveAction.ReadValue<Vector2>();
+    move = MoveAction.ReadValue<Vector2>();
+     Debug.Log(move); 
+    }
 
-      
-      Debug.Log(move); 
-
-
-      Vector2 position = (Vector2)transform.position + move * 8.0f * Time.deltaTime;
-      transform.position = position;  
+    void FixedUpdate()
+    {
+      Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+      Rigidbody2D.MovePosition(position);  
     }
 }     
